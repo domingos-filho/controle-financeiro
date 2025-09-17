e894faeaf6a7bfd3f7116bd45209916a78d57dac
Francisco Antonio Domingos Filho <50499579+domingos-filho@users.noreply.github.com>
2025-09-17 13:00:53 -0300

    Delete finance_app directory

--- finance_app/frontend/service-worker.js
+++
@@ -1,9 +0,0 @@
-// basic service worker caching
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open('finance-cache-v1').then(cache => cache.addAll(['/','/index.html','/styles.css','/app.js','/manifest.json'])));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
-});
